9da50ff6c6cad5055bfae3f187d5c1593bbff53f
Asgar Ali Rizvi <asgar.ali.rizvi@fxdtechnologies.com>
2025-08-12 17:51:48 +0530

    Latest Project Updated

---
+++ first-nextjs-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl flex items-center justify-center">
+        Welcome to Nextjs
+      </h1>
+
+      <div className="text-bold">Hello World!</div>
+    </>
+  );
+}
